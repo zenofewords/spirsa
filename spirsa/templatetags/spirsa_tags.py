a788f6a2b92a9dbb70ad979a479ff6486f14deb0
Dominik Zen <zen.dominik@gmail.com>
2020-10-17 21:22:07 +0200

    Add hover for desktop artwork

--- spirsa/templatetags/spirsa_tags.py
+++ spirsa/templatetags/spirsa_tags.py
@@ -1,5 +1,6 @@
 from django import template
 
+from art.models import Artwork
 from spirsa.constants import HOME_URL_NAME
 
 register = template.Library()
@@ -7,8 +8,19 @@ register = template.Library()
 
 @register.inclusion_tag('spirsa/tags/menu_link_tag.html')
 def menu_link_tag(name, path, base=''):
+    split_path = list(filter(None, path.split('/')))
+    detail = None
+
+    if len(split_path) > 1:
+        artwork = Artwork.objects.filter(slug=split_path.pop()).first()
+
+        if artwork and name == 'digital':
+            detail = not artwork.is_traditional
+        if artwork and name == 'traditional':
+            detail = artwork.is_traditional
+
     return {
         'name': name,
         'reverse_url': '{}:{}'.format(base, name) if base else name,
-        'current': name in path or name == HOME_URL_NAME and '/' == path,
+        'current': name in path or detail or name == HOME_URL_NAME and '/' == path,
     }
