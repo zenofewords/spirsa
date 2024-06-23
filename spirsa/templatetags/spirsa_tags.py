55104ce167b1fffed90998b87cb7eec929211772
Dominik Zen <zen.dominik@gmail.com>
2024-06-23 11:22:19 +0200

    Rework navigation

--- spirsa/templatetags/spirsa_tags.py
+++ spirsa/templatetags/spirsa_tags.py
@@ -7,22 +7,20 @@ register = template.Library()
 
 
 @register.inclusion_tag('spirsa/tags/menu_link_tag.html')
-def menu_link_tag(name, path, dynamic=False):
-    url = name.replace('/', '-')
+def menu_link_tag(title, slug, path, dynamic=False):
     return {
-        'name': name.replace('-', ' '),
-        'url': reverse(url) if dynamic else '/{}'.format(url),
-        'current': url in path,
+        'title': title,
+        'url': reverse(slug) if dynamic else '/{}'.format(slug),
+        'current': slug in path,
     }
 
 
 @register.inclusion_tag('spirsa/tags/navigation_tag.html', takes_context=True)
 def navigation_tag(context, mobile=False):
     request = context.get('request')
-    collections = Collection.objects.for_navigation()
 
     return {
         'mobile': mobile,
         'request_path': request.path,
-        'navigation_slugs': [collection.slug for collection in collections]
+        'collections': Collection.objects.for_navigation()
     }
